29173416f925a0c3c948b2a921dd7851d4e26eae
happyjy <okwoyjy@gmail.com>
2018-07-24 23:08:14 +0900

    complete todolist function

--- todolist/app.js
+++ todolist/app.js
@@ -13,8 +13,6 @@ var app = new Vue({
   }
 });
 
-
-
 const items = JSON.parse(localStorage.getItem('items')) || [];
 const addItems = document.querySelector('.add-items');
 const toDoList = document.querySelector('.toDoList');
@@ -23,25 +21,41 @@ function addItem(e){
   e.preventDefault(); //won't refresh browser
 
   const text = (this.querySelector('[name=item]')).value;
-  const item = { text };
+  const item = { 
+    text,
+    done: false
+   };
   console.log("### name = item value : " + this.querySelector('[name=item').value);
-
   items.push(item);
-  // addItemAtList(items || [], addItems);
+  addListLi(items, toDoList);
   localStorage.setItem('toDoList', JSON.stringify(items));
-  // toDoList.innerHTML = `<li> dd </li>`;  
-  toDoList.innerHTML = items.map((item, i)=>{
-    return ` <li>
-      <label>${item.text}</label>
-    </li>
-    `
-  }).join('');
   this.reset;// this : document.querySelector('.add-items');
 }
 
-// function addItemAtList(items, toDoList){
-//   toDoList.innerHTML = items.
-// }
+function addListLi(items = [], toDoList){
+  // toDoList.innerHTML = `<li> dd </li>`;  
+  toDoList.innerHTML = items.map((item, i)=>{
+    // <label>${item.text}</label>
+    return ` <li>
+      <input type="checkbox" data-index=${i} 
+      id="item${i}"
+      ${item.done ? 'checked' : ''}/>
+      <label for="item${i}"> ${item.text} </label>
+    </li>
+    `
+  }).join('');
+}
+
+function toggleDone(e){
+  console.log(e.target);
+  if(!e.target.matches('input')) return;
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem('items', JSON.stringify(items));
+  addListLi(items, toDoList);
+}
 
 
 addItems.addEventListener('submit', addItem);
+toDoList.addEventListener('click', toggleDone);
